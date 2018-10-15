10111a0e706c21d7f6e81ee0045b0f648ff4c629
Rex1021 <234793928@qq.com>
2018-10-15 17:34:53 +0800

    route settings

--- ec/models.py
+++ ec/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
 
 
 class ExcelTable(models.Model):
-    name = models.CharField(max_length=45)
+    name = models.CharField(max_length=45, unique=True)
     presentation = models.CharField(max_length=300)
--- ec/views.py
+++ ec/views.py
@@ -1,7 +1,10 @@
+from django.db import IntegrityError
+from django.http import HttpResponseRedirect, HttpResponseServerError
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
 from ec import models
+
 
 # Create your views here.
 
@@ -10,11 +13,29 @@ def index(request):
     # request.POST
     # request.GET
     # return HttpResponse("Hello word")
+    if "/index/" == request.path:
+        return find_all_tables(request)
+    if "/index/createTable" == request.path:
+        return create_table(request)
+    return HttpResponse("something wrong")
 
-    print(request.path)
+
+def find_all_tables(request):
     tables = models.ExcelTable.objects.all()
     return render(request, "index.html", {"tables": tables})
 
-# def createTable(request):
-#
+
+def create_table(request):
+    title = request.POST.get("title")
+    presentation = request.POST.get("presentation")
+    print(title, presentation)
+    try:
+        table = models.ExcelTable.objects.create(name=title, presentation=presentation)
+        table.save()
+        return HttpResponseRedirect("/index/")
+    except IntegrityError as msg:
+        print(msg)
+        return HttpResponse("<javascript>alert('1111')<javascript>")
+
+
 
